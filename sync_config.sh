d7504be2178c09e407eeb7006e9d83f901a3b315
Hung <hung.nd.4work@gmail.com>
2023-06-17 15:10:32 +0700

    ok

--- sync_config.sh
+++ sync_config.sh
@@ -10,6 +10,9 @@ destination_directory="$HOME/arch/cfg"
 files=(
     ".bashrc"
     ".vim"
+)
+
+dot_config=(
     ".config/hypr"
     ".config/alacritty"
     ".config/waybar"
@@ -17,6 +20,11 @@ files=(
 
 # Sync files and folders
 for item in "${files[@]}"; do
-    rsync -avz --delete "$source_directory/$item" "$destination_directory"
+    rsync -avr --delete "$source_directory/$item" "$destination_directory"
+done
+
+# Sync files and folders
+for item in "${dot_config[@]}"; do
+    rsync -avr --delete "$source_directory/$item" "$destination_directory/.config"
 done
 
